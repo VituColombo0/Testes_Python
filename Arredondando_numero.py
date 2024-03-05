af0557e874e1854395f16dca88129d9fb5f3a79d
Victor Vieira Colombo <victorcolombo03@gmail.com>
2024-03-05 17:32:02 -0300

    .

--- Arredondando_numero.py
+++ Arredondando_numero.py
@@ -6,3 +6,9 @@ calc = math.floor(num)
 print('O valor {} de forma inteira é {}'.format(num, calc))
 
 # floor arredonda para baixo e ceil para cima
+
+# Sem o uso de bibliotecas:
+
+'''num = float(input('Insira um número quebrado: '))
+print('O valor digitado foi {} e sua parte inteira é {}'.format(num, int(num)))'''
+
