8cd53c2db3343bb02ca96c419f116c0fa00aa612
Victor Vieira Colombo <victorcolombo03@gmail.com>
2024-03-05 21:40:14 -0300

    .

--- Analisar_Nomes.py
+++ Analisar_Nomes.py
@@ -10,7 +10,29 @@ separa = nome.split()
 print('Seu nome em maiúsculo é: {}'.format(nome.upper()))
 print('Seu nome em minúsculo é: {}'.format(nome.lower()))
 
-print('Seu nome ao todo tem {} letras'.format(len(nome) - nome.count(' ')))
 
-print('Seu primeiro nome é {}, e ele tem {} letras'.format(separa[0] , len(separa[0])))
+print('Seu nome ao todo tem {} letras'.format(len(nome) - nome.count(' '))) # elimina os espaços entre as palavras
+
+print('Seu primeiro nome é {}, e ele tem {} letras'.format(separa[0], len(separa[0])))
+
+
+
+
+
+#REVISAR ISSO, ENTENDER O QUE ESTÁ ACONTECENDO EM CADA LINHA
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
