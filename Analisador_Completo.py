2e33e9a152a90d1a1dd640758ceca828f49275a4
Victor Vieira Colombo <161251705+VituColombo0@users.noreply.github.com>
2024-03-18 13:54:49 -0300

    .

--- Analisador_Completo.py
+++ Analisador_Completo.py
@@ -1,8 +1,5 @@
-somaidade = 0
-mediaidade = 0
-maioridadehomem = 0
+somaidade = mediaidade = maioridadehomem = totmulher20 = 0 
 nomevelho = ''
-totmulher20 = 0
 
 for p in range(1, 5):
     print('----- {}° PESSOA -----'.format(p))
