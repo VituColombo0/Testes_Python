5401a22b6645413c59123fb6432d238b79242d78
Victor Vieira Colombo <161251705+VituColombo0@users.noreply.github.com>
2024-03-18 13:55:20 -0300

    .

--- Analisar_Nomes.py
+++ Analisar_Nomes.py
@@ -1,8 +1,3 @@
-'''Programa que leia o nome completo e mostre:
-
-- Nome todo em maiúsculo/minúsculo
-- Quantas letras ao todo, tirando todos os espaços
-- Quantas letras tem o primeiro nome'''
 
 nome = str(input('Digite seu nome completo: ')).strip()
 separa = nome.split()
@@ -14,12 +9,6 @@ print('Seu nome em minúsculo é: {}'.format(nome.lower()))
 print('Seu nome ao todo tem {} letras'.format(len(nome) - nome.count(' '))) # elimina os espaços entre as palavras
 
 print('Seu primeiro nome é {}, e ele tem {} letras'.format(separa[0], len(separa[0])))
-
-
-
-
-
-#REVISAR ISSO, ENTENDER O QUE ESTÁ ACONTECENDO EM CADA LINHA
 
 
 
