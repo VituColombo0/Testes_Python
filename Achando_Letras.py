490886a25f643340d8f0ec7235c408d71d6ecd74
Victor Vieira Colombo <161251705+VituColombo0@users.noreply.github.com>
2024-04-02 16:04:43 -0300

    Update Achando_Letras.py

--- Achando_Letras.py
+++ Achando_Letras.py
@@ -2,3 +2,5 @@ letra = str(input('Insira uma frase:')).upper().strip()
 
 print('A letra "A" aparece {} vezes na frase'.format(letra.count("A")))
 print('A primeira letra "A" apareceu na posição {}'.format(letra.rfind('A')))
+
+#1
